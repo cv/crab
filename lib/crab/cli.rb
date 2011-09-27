3c70a390168a6f65d6cd63e4985b63527da2a3bd
Carlos Villela <cvillela@thoughtworks.com>
2011-09-27 19:20:43 -0300

    refactor: removing case statement, using reflection inside Crab::Command

--- lib/crab/cli.rb
+++ lib/crab/cli.rb
@@ -23,41 +23,10 @@ crab version #{Crab::VERSION}: A Cucumber-Rally bridge
       end
 
       cmd = ARGV.shift # get the subcommand
-      case cmd
-      when "pull"
-        Crab::Commands::Pull.new(global_opts, ARGV).run
+      Trollop::die "Unknown subcommand" unless cmd
+      Trollop::die "Unknown subcommand #{cmd.inspect}" unless Crab::Commands.const_defined? cmd.capitalize
 
-      when "show"
-        Crab::Commands::Show.new(global_opts, ARGV).run
-
-      when "login"
-        Crab::Commands::Login.new(global_opts, ARGV).run
-
-      when "update"
-        Crab::Commands::Update.new(global_opts, ARGV).run
-
-      when "find"
-        Crab::Commands::Find.new(global_opts, ARGV).run
-
-      when "project"
-        Crab::Commands::Project.new(global_opts, ARGV).run
-
-      when "create"
-        Crab::Commands::Create.new(global_opts, ARGV).run
-
-      when "delete"
-        Crab::Commands::Delete.new(global_opts, ARGV).run
-
-      when "testcase"
-        Crab::Commands::Testcase.new(global_opts, ARGV).run
-
-      else
-        if cmd
-          Trollop::die "Unknown subcommand #{cmd.inspect}"
-        else
-          Trollop::die "Unknown subcommand"
-        end
-      end
+      Crab::Commands.const_get(cmd.capitalize).new(global_opts, ARGV).run
     end
   end
 end
