84252588d93d4dc8e6661f057dd62c5da4bff99b
Carlos Villela <cvillela@thoughtworks.com>
2011-09-28 00:17:55 -0300

    sort

--- lib/crab.rb
+++ lib/crab.rb
@@ -1,31 +1,31 @@
 require "crab/version"
 
 # common dependencies
-require 'trollop'
+require 'active_support/all'
 require 'fileutils'
 require 'gherkin/i18n'
 require 'highline/import'
-require 'active_support/all'
 require 'rally_rest_api'
 require 'sanitize'
+require 'trollop'
 
 # internals
-require "crab/utilities"
+require "crab/cli"
 require "crab/rally"
 require "crab/story"
 require "crab/testcase"
-require "crab/cli"
+require "crab/utilities"
 
 # supported commands
-require "crab/commands/update"
-require "crab/commands/pull"
-require "crab/commands/find"
-require "crab/commands/login"
-require "crab/commands/show"
-require "crab/commands/project"
 require "crab/commands/create"
 require "crab/commands/delete"
+require "crab/commands/find"
+require "crab/commands/login"
+require "crab/commands/project"
+require "crab/commands/pull"
+require "crab/commands/show"
 require "crab/commands/testcase"
+require "crab/commands/update"
 
 # cucumber support
 require "crab/cucumber_feature"
