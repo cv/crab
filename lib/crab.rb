3e4c279a980efbd2d1126ae0774748652b21e1b9
Carlos Villela <cvillela@thoughtworks.com>
2011-10-02 19:32:41 -0300

    initial impl of diff -- can diff names and count of test cases so far

--- lib/crab.rb
+++ lib/crab.rb
@@ -3,6 +3,7 @@ require "crab/version"
 # common dependencies
 require 'active_support/all'
 require 'fileutils'
+require 'gherkin'
 require 'gherkin/i18n'
 require 'highline/import'
 require 'rally_rest_api'
@@ -14,6 +15,7 @@ require "crab/utilities"
 require "crab/rally"
 require "crab/story"
 require "crab/testcase"
+require "crab/cucumber_to_rally_adapter"
 
 # cucumber support
 require "crab/cucumber_feature"
---
+++ lib/crab/cucumber_to_rally_adapter.rb
@@ -0,0 +1,44 @@
+class Crab::CucumberToRallyAdapter
+
+  def initialize(feature_text, feature_file)
+    @scenarios = []
+    @steps = ActiveSupport::OrderedHash.new
+
+    parser = Gherkin::Parser::Parser.new(self, false, "root", false)
+    parser.parse feature_text, feature_file, 0
+  end
+
+  attr_reader :scenarios, :steps
+
+  # Rally compat
+  def story_id
+    @feature.name.match(/^\[([^\]]+)\](.*)$/)
+    $1.strip
+  end
+
+  def name
+    @feature.name.match(/^\[([^\]]+)\](.*)$/)
+    $2.strip
+  end
+
+  # Cucumber compat
+  def uri(uri)
+    @uri
+  end
+
+  def feature(feature)
+    @feature = feature
+  end
+
+  def scenario(scenario)
+    @scenarios << scenario
+  end
+
+  def step(step)
+    @steps[@scenarios.last] += Array(step)
+  end
+
+  def eof
+  end
+
+end
